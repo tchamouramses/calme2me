9c421bbdda3789d0f760a47aaca81a0b2922e842
tchamouramses <tchamouramses@gmail.com>
2026-02-18 23:53:59 -0500

    update

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,14 +10,12 @@ use Illuminate\Support\Facades\Route;
 Route::post('/problems', [ProblemController::class, 'store'])->middleware('suspension');
 // Auth routes
 Route::post('/problems/{problem}/comments', [CommentController::class, 'store'])->middleware('suspension');
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+Route::post('/login', [AuthController::class, 'login']);
 
 // Public routes
 Route::get('/problems', [ProblemController::class, 'index']);
 Route::get('/problems/{problem:uuid}', [ProblemController::class, 'show']);
-Route::post('/problems', [ProblemController::class, 'store']);
-    Route::post('/admin/rejections/{rejectedMessage}/suspend', [RejectedMessageController::class, 'suspend']);
+Route::post('/admin/rejections/{rejectedMessage}/suspend', [RejectedMessageController::class, 'suspend']);
 Route::get('/problems/{problem}/comments', [CommentController::class, 'index']);
 Route::post('/problems/{problem}/comments', [CommentController::class, 'store']);
 Route::post('/problems/{problem}/reactions', [ProblemReactionController::class, 'toggle']);
@@ -25,6 +23,8 @@ Route::post('/comments/{comment}/reactions', [CommentReactionController::class, 
 
 // Admin routes (protected)
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/admin/problems', [ProblemController::class, 'adminIndex']);
     Route::get('/admin/rejections', [RejectedMessageController::class, 'index']);
     Route::patch('/problems/{problem}/status', [ProblemController::class, 'updateStatus']);
