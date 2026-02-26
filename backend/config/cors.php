492bebc6777dd95b8cc46fc7782c2e079ff745bc
Ramses Tchamou <57516744+tchamouramses@users.noreply.github.com>
2026-02-25 21:24:57 -0500

    Add additional allowed origin for CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://www.calme2me.com', 'http://localhost:5173'],
+    'allowed_origins' => ['https://www.calme2me.com', 'https://calme2me.com',  'http://localhost:5173'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
